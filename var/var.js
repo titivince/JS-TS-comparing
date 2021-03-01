81d1243870138fd19b367de53c9a1249145c9166
titivince <72084738+titivince@users.noreply.github.com>
2021-03-01 16:04:39 +0100

    add var

---
+++ var/var.js
@@ -0,0 +1,5 @@
+var n;
+var s, // the ts compiler transform the let in var
+    b, 
+    nul;
+var NUL = null; // the ts complier will transform the const in var
---
+++ var/var.ts
@@ -0,0 +1,5 @@
+var n:number;
+let s:string, // the ts compiler will transform the let in var
+    b:boolean,
+    nul:null;
+const NUL = null; // the ts complier will transform the const in var
